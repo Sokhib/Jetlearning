b80a3bf5ac8229d1f176347e6dd610f8780acb58
Sokhib <sokhibsaid@gmail.com>
2022-09-15 01:41:27 +0400

    FancyLikeButton, OrangeBoxSize anim AvatarView with circle gradient Selectable item TrackView

--- app/src/main/java/com/example/jetlearning/MainActivity.kt
+++ app/src/main/java/com/example/jetlearning/MainActivity.kt
@@ -4,16 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetlearning.ui.theme.JetlearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,15 +41,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetlearningTheme {
-                var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.primary
                 ) {
-                    if (shouldShowOnboarding)
-                        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-                    else
-                        Greeting(name = "Compose")
+
+                    FancyLike()
                 }
             }
         }
@@ -37,58 +53,114 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
-
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
-            ) {
-                Text("Continue")
+fun FancyLike() {
+    var isLiked by remember {
+        mutableStateOf(false)
+    }
+    var filledLikeScale by remember {
+        mutableStateOf(0f)
+    }
+    var showEmptyLike by remember {
+        mutableStateOf(true)
+    }
+    LaunchedEffect(isLiked) {
+        if (isLiked) {
+            animate(
+                0f, 1.0f, animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            ) { value, _ ->
+                filledLikeScale = value
+                if (value >= 1f) showEmptyLike = false
             }
+        }
+    }
+    if (showEmptyLike) {
+        Icon(Icons.Default.FavoriteBorder, "", Modifier.clickable {
+            isLiked = !isLiked
+        })
+    }
+    if (filledLikeScale > 0f) {
+        Icon(
+            Icons.Default.Favorite,
+            "",
+            Modifier
+                .scale(filledLikeScale)
+                .clickable {
+                    filledLikeScale = 0f
+                    isLiked = !isLiked
+                    showEmptyLike = true
+                })
+    }
+}
+
+
+@Composable
+fun OrangeBox() {
+    var big by remember {
+        mutableStateOf(false)
+    }
+    Box(modifier = Modifier
+        .padding(10.dp)
+        .border(4.dp, Color.Black, CircleShape)
+        .clickable { big = !big }
+        .size(animateDpAsState(targetValue = if (big) 128.dp else 64.dp).value)
+        .clip(RoundedCornerShape(24.dp))
+    )
+}
+
+
+@Composable
+fun AvatarView() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_andr_icon),
+        contentDescription = "",
+        modifier = Modifier
+            .padding(16.dp)
+            .border(2.dp, Brush.sweepGradient(listOf(Color.Yellow, Color.Cyan)), CircleShape)
+            .border(4.dp, Color.White, CircleShape)
+            .clip(CircleShape)
+            .clickable {}
+    )
+}
+
+@Composable
+fun TrackList(tracks: List<Int> = listOf(1, 2, 3, 4, 5)) {
+    var selectedTrack by rememberSaveable { mutableStateOf<Int?>(null) }
+    LazyColumn {
+        items(tracks) { track ->
+            Track(
+                isSelectedTrack = track == selectedTrack,
+                track = track,
+                onClick = { selectedTrack = track }
+            )
         }
     }
 }
 
 @Composable
-private fun Greeting(name: String) {
-
-    var expanded by remember { mutableStateOf(false) }
-
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
+fun Track(
+    isSelectedTrack: Boolean,
+    track: Int,
+    onClick: () -> Unit
+) {
+    Row(modifier = Modifier.clickable(onClick = onClick)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_airplay_24),
+            colorFilter = ColorFilter.tint(if (isSelectedTrack) Color.Green else Color.Black),
+            contentDescription = "queen",
+            modifier = Modifier
+                .padding(8.dp)
+                .size(56.dp)
         )
-    )
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier
-            .padding(vertical = 4.dp, horizontal = 8.dp)
-            .wrapContentSize()
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name, style = MaterialTheme.typography.h4)
-            }
-            OutlinedButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
-            }
-
+        Column(modifier = Modifier.padding(vertical = 8.dp)) {
+            Text(text = "$track")
+            Text(
+                text = "Queen",
+                fontSize = 14.sp,
+                color = Color.Black.copy(alpha = 0.5f)
+            )
         }
     }
 }
@@ -97,6 +169,6 @@ private fun Greeting(name: String) {
 @Composable
 fun OnboardingPreview() {
     JetlearningTheme {
-        OnboardingScreen(onContinueClicked = {})
+        TrackList()
     }
 }
