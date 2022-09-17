575ca7a90a996b78e9937b89e0618686613be654
Sokhib <sokhibsaid@gmail.com>
2022-09-18 00:29:35 +0400

    Recomposition test

--- app/src/main/java/com/example/jetlearning/MainActivity.kt
+++ app/src/main/java/com/example/jetlearning/MainActivity.kt
@@ -3,17 +3,13 @@ package com.example.jetlearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.jetlearning.ui.theme.JetlearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,82 +17,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetlearningTheme {
-                var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    if (shouldShowOnboarding)
-                        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-                    else
-                        Greeting(name = "Compose")
+                    var user by remember {
+                        mutableStateOf(User("So", "Soft Dev"))
+                    }
+                    GreetingCard(user) {
+                        user = User(Math.random().toString(), "Android Dev")
+                    }
                 }
             }
         }
     }
 }
 
+// If value is not changed, particular composable is not going through recomposition
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
-
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
-            ) {
-                Text("Continue")
-            }
-        }
-    }
+fun GreetingCard(user: User, onClick: () -> Unit) {
+    Text(text = user.name)
+    Greeting(user, onClick)
 }
 
 @Composable
-private fun Greeting(name: String) {
-
-    var expanded by remember { mutableStateOf(false) }
-
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier
-            .padding(vertical = 4.dp, horizontal = 8.dp)
-            .wrapContentSize()
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name, style = MaterialTheme.typography.h4)
-            }
-            OutlinedButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
-            }
-
-        }
+fun Greeting(user: User, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(text = user.title)
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    JetlearningTheme {
-        OnboardingScreen(onContinueClicked = {})
-    }
-}
+data class User(var name: String, var title: String)
