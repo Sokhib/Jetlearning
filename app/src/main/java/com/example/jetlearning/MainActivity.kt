ace213f84b91216a87737ec0dd7db2d2fb4754a3
Sokhib <sokhibsaid@gmail.com>
2022-09-22 01:37:40 +0400

    LazyLayout simple example

--- app/src/main/java/com/example/jetlearning/MainActivity.kt
+++ app/src/main/java/com/example/jetlearning/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.jetlearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import com.example.jetlearning.ui.theme.JetlearningTheme
 
@@ -21,82 +24,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetlearningTheme {
-                var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    if (shouldShowOnboarding)
-                        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-                    else
-                        Greeting(name = "Compose")
+                    TitlesOfBook()
                 }
             }
         }
     }
 }
 
-@Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
-            ) {
-                Text("Continue")
-            }
+@Composable
+fun TitlesOfBook() {
+    LazyColumn {
+        val books = listOf("Zero to One", "Otets", "Compose Cookbook")
+        item {
+            Text(text = "List of Books", modifier = Modifier.size(56.dp), color = Color.Magenta)
         }
-    }
-}
-
-@Composable
-private fun Greeting(name: String) {
-
-    var expanded by remember { mutableStateOf(false) }
-
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier
-            .padding(vertical = 4.dp, horizontal = 8.dp)
-            .wrapContentSize()
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
+        items(books) { book ->
+            Text(text = book)
+        }
+        item {
+            Text(
+                text = "Tv Shows",
                 modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name, style = MaterialTheme.typography.h4)
-            }
-            OutlinedButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
-            }
-
+                    .padding(16.dp)
+                    .size(56.dp),
+                color = Color.Red,
+                fontStyle = FontStyle.Italic,
+                fontFamily = FontFamily.Serif
+            )
         }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    JetlearningTheme {
-        OnboardingScreen(onContinueClicked = {})
+        val tvShows = listOf("Sambaqa", "Jerbaqa")
+        items(tvShows) { tvShow ->
+            Text(text = tvShow)
+        }
     }
 }
