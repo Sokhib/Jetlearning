fd8618ae7e7603765c1f3bc57737ab392d3246e1
Sokhib <sokhibsaid@gmail.com>
2022-10-24 12:21:20 +0400

    Bounce click and press animation

--- app/src/main/java/com/example/jetlearning/MainActivity.kt
+++ app/src/main/java/com/example/jetlearning/MainActivity.kt
@@ -3,15 +3,23 @@ package com.example.jetlearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetlearning.ui.theme.JetlearningTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetlearningTheme {}
+            JetlearningTheme {
+                PulsateEffect()
+            }
         }
     }
 }
@@ -20,5 +28,19 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun OnboardingPreview() {
-    JetlearningTheme {}
+    JetlearningTheme {
+        PulsateEffect()
+    }
+}
+
+@Composable
+fun PulsateEffect() {
+    Button(
+        onClick = {},
+        shape = RoundedCornerShape(12.dp),
+        contentPadding = PaddingValues(16.dp),
+        modifier = Modifier.pressClick()
+    ) {
+        Text(text = "Click Me")
+    }
 }
