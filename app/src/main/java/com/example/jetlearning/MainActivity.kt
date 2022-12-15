ae2efc534eb72210cffe00517e0c67044cadaead
Sokhib <sokhibsaid@gmail.com>
2022-12-15 20:31:58 +0400

    Upgrade AGP dependency from 7.2.2 to 7.3.1  Upgrade Gradle version to 7.4  Move package from Android manifest to build files
     Add Animated Visibility of Button

--- app/src/main/java/com/example/jetlearning/MainActivity.kt
+++ app/src/main/java/com/example/jetlearning/MainActivity.kt
@@ -4,14 +4,26 @@ import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -70,4 +82,32 @@ fun PulsateEffect() {
             )
         }
     }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun AnimateVisibilityExample() {
+    var isVisible by remember {
+        mutableStateOf(false)
+    }
+
+    Column {
+        Button(
+            onClick = { isVisible = !isVisible },
+            modifier = Modifier
+                .wrapContentHeight()
+                .wrapContentWidth()
+        ) {
+            Text("Click me")
+        }
+
+        AnimatedVisibility(visible = isVisible, enter = fadeIn() + slideInHorizontally()) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Red)
+                    .size(200.dp)
+            )
+        }
+    }
+
 }
