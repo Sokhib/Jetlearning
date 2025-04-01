328837cf32c0e8173d5084b374f482e0f9b655f9
Sokhib <sokhibsaid@gmail.com>
2025-04-01 12:23:58 +0400

    Lesson 2 with details

--- app/src/main/java/com/example/jetlearning/MainActivity.kt
+++ app/src/main/java/com/example/jetlearning/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.jetlearning
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetlearning.ui.theme.JetlearningTheme
 
@@ -11,14 +18,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetlearningTheme {}
+            JetlearningTheme {
+                CustomLogButton()
+            }
         }
     }
 }
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun OnboardingPreview() {
-    JetlearningTheme {}
+    JetlearningTheme {
+        CustomLogButton()
+    }
+}
+
+@Composable
+fun CustomLogButton() {
+    Log.d("TAG", "1") //Only first time
+    var counter by remember {
+        Log.d("TAG", "2") // Only first time
+        mutableStateOf(0)
+    }
+    Button(onClick = { counter += 1 }) {
+        Log.d("TAG", "3") // Every time, but why
+        // (Because state change could affect other composables,
+        // that's why all block inside button (in which counter is used) gets recomposition.)
+        Text(text = "Just Static Text ")
+        Text(text = "$counter")
+    }
 }
